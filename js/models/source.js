1caff6630ca0bebac3894ad1de5155005fe3bad4
lizkalina <liz.kalina@gmail.com>
2016-04-01 16:34:20 -0400

    adds adapters

--- js/models/source.js
+++ js/models/source.js
@@ -1,1 +1,15 @@
 // adapter model?
+app.source = {
+  all: [],
+  new: (function() {
+    var counter = 0;
+    return function Source(response) {
+      this.response = response;
+      var self = this;
+      (function init(){
+        self.id = ++counter;
+        app.source.all.push(self);
+      }());
+    }
+  }())
+}
