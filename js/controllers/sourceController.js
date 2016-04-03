47c23576aad3aec076b5b30a48b961a8fd7c76db
annafractuous <anna.nigma@yahoo.com>
2016-04-03 03:07:30 -0400

    Add styling: jumbotron gif, nice font, styled form

--- js/controllers/sourceController.js
+++ js/controllers/sourceController.js
@@ -2,25 +2,24 @@ app.source.controller = {
   init : function(){
     $('#submit').on('click',function(event){
       event.preventDefault();
-      var topic = $('#haiku_topic').val();
 
+      var topic = $('#haiku_topic').val();
       app.source.adapter.getBy(topic)
       .then(function(sourceObj){
-        // sourceObj = { extract: "Eleanor Kagan....", id: 1, topic: "Billie Holiday" }
         return app.word.adapter.queryWords(sourceObj)
       })
       .then(function(sourceId){
         return app.haiku.generate(sourceId);
       })
       .then(function(haiku){
-        debugger
-        _.each(haiku.lines, function(line) {
-          $('.haikus').append("<li>" + line + "</li>")
-        });
+        app.source.controller.render(haiku);
       })
     })
   },
-  render : function(){
-
+  render : function(haiku){
+    $('.haikus').text("");
+    _.each(haiku.lines, function(line) {
+      $('.haikus').append("<p>" + line + "\n </p>");
+    })
   }
 }
