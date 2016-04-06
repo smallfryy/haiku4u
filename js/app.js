6b480ac953fc8f7d58e98d4363ec847e5f32f120
annafractuous <anna.nigma@yahoo.com>
2016-04-05 22:42:46 -0400

    Make 'About' section close on click, fit to match content height, and style font

--- js/app.js
+++ js/app.js
@@ -5,7 +5,16 @@ $(function() { // on document ready
   app.controller.init();
 
   $("#footer").click(function(){
-    $('#about').slideToggle("slow");
+    var available_space = $('html').height() - $('.jumbotron').outerHeight();
+    $('#about').css({
+      height: available_space,
+    });
   });
-    
+
+  $('#about').click(function(){
+    $('#about').css({
+      height: '0px'
+    });
+  });
+
 });
